188945c343330e7cb62a3da5e5e78eb98f818ac4
pumpurrnickel <denasanch1999@gmail.com>
2022-04-11 00:08:42 -0400

    added visibility anims and future forcasts

--- index.js
+++ index.js
@@ -47,12 +47,70 @@ function searchZip() {
           document.querySelector(".high").innerHTML = `High: ${high}`;
 
           let humidityInput = weatherJson.current.humidity;
-          document.querySelector(".humidity").innerHTML = `Humidity: ${humidityInput}`;
+          document.querySelector(
+            ".humidity"
+          ).innerHTML = `Humidity: ${humidityInput}`;
 
           let windInput = Math.round(weatherJson.current.wind_speed);
-          document.querySelector(".wind").innerHTML = `Wind Speed: ${windInput}mph`;
+          document.querySelector(
+            ".wind"
+          ).innerHTML = `Wind Speed: ${windInput}mph`;
 
           document.querySelector(".current").classList.add("visible");
+
+          //   Tomorrow
+
+          let tomorrowConditions = weatherJson.daily[0].weather[0].main;
+          document.querySelector(
+            ".tomConditions"
+          ).innerHTML = `${tomorrowConditions}`;
+          let tomHi = Math.round(weatherJson.daily[0].temp.min);
+          document.querySelector(".tomHi").innerHTML = `High: ${tomHi} / `;
+          let tomLow = Math.round(weatherJson.daily[0].temp.max);
+          document.querySelector(".tomLow").innerHTML = `Low: ${tomLow}`;
+
+          // Two Days
+
+          let twoDayConditions = weatherJson.daily[1].weather[0].main;
+          document.querySelector(
+            ".twoDayConditions"
+          ).innerHTML = `${twoDayConditions}`;
+          let twoDayHi = Math.round(weatherJson.daily[1].temp.min);
+          document.querySelector(".twoDayHi").innerHTML = `High: ${twoDayHi} / `;
+          let twoDayLow = Math.round(weatherJson.daily[1].temp.max);
+          document.querySelector(".twoDayLow").innerHTML = `Low: ${twoDayLow}`;
+
+          // Three Days
+
+          let threeDayConditions = weatherJson.daily[2].weather[0].main;
+          document.querySelector(
+            ".threeDayConditions"
+          ).innerHTML = `${threeDayConditions}`;
+          let threeDayHi = Math.round(weatherJson.daily[2].temp.min);
+          document.querySelector(".threeDayHi").innerHTML = `High: ${threeDayHi} / `;
+          let threeDayLow = Math.round(weatherJson.daily[2].temp.max);
+          document.querySelector(".threeDayLow").innerHTML = `Low: ${threeDayLow}`;
+
+          document.querySelector(".future").classList.add("visible");
+
+          document.querySelector(".tom").classList.add("visible");
+          document.querySelector(".twoDays").classList.add("visible");
+          document.querySelector(".threeDays").classList.add("visible");
+
+          // Icons
+
+          let todayIcon = weatherJson.current.weather[0].icon;
+          let tomorrowIcon = weatherJson.daily[1].weather[0].icon;
+          let twoDayIcon = weatherJson.daily[1].weather[0].icon;
+          let threeDayIcon = weatherJson.daily[1].weather[0].icon;
+
+
+          document.querySelector(".todayIcon").src=`https://openweathermap.org/img/wn/${todayIcon}.png`;
+          document.querySelector(".tomIcon").src=`https://openweathermap.org/img/wn/${tomorrowIcon}.png`;
+          document.querySelector(".twoDayIcon").src=`https://openweathermap.org/img/wn/${twoDayIcon}.png`;
+          document.querySelector(".threeDayIcon").src=`https://openweathermap.org/img/wn/${threeDayIcon}.png`;
+
+
 
         });
     });
